d76e18004f7bed41e04080f871dd7b6a808785a0
Mustehsan <smustehsan45@gmail.com>
2026-06-08 14:11:34 +0500

    initial commi2t

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
   "https://yusratuitionlab-elu5.vercel.app",
-  "https://yusratuitionlab.onrender.com",
+  
 ];
 app.use(
   cors({
