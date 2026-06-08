b1e16da32f842441ff75db2ad0d56282266e06b6
Mustehsan <smustehsan45@gmail.com>
2026-06-08 14:50:47 +0500

    initial comm4it

--- backend/server.js
+++ backend/server.js
@@ -28,13 +28,7 @@ const allowedOrigins = [
 ];
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("CORS policy blocked the request."));
-      }
-    },
+    origin: true,
     credentials: true,
   })
 );
